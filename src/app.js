9da5023234ce8b28371c14d9b33aed7dcaa3abd7
drkchiloll <samwomack_@me.com>
2016-06-16 15:20:47 -0500

    Added Methods to inject into the Sidebar in order to utilize the State/Store of the Application Used a React Lifecycle method to add Focus on Input Field when it is "triggered" on (visible) in the view (when you want to add a new deck by pressing the button that was added in the Sidebar)

--- src/app.js
+++ src/app.js
@@ -60,36 +60,51 @@ const App = (props) => {
 };
 
 class Sidebar extends React.Component {
-
+  componentDidUpdate() {
+    var el = this.refs.add;
+    if(el) el.focus();
+  }
   render() {
     let props = this.props;
     return (
       <div className='sidebar'>
         <h2>All Decks</h2>
-
+        <button onClick={ e => this.props.showAddDeck() }>
+          New Deck
+        </button>
         <ul>
           {props.decks.map((deck, i) =>
             <li key={i}> { deck.name } </li>
           )}
         </ul>
-        { props.addingDeck && <input ref='add' /> }
+        { props.addingDeck &&
+          <input ref='add' onKeyPress={this.createDeck.bind(this)} /> }
       </div>
     )
+  }
+  createDeck(evt) {
+    if(evt.which !== 13) return;
+    var name = this.refs.add.value;
+    this.props.addDeck(name);
+    this.props.hideAddDeck();
   }
 }
 
 function run() {
   let state = store.getState();
+  console.log(state);
   ReactDOM.render(
     <App>
-      <Sidebar decks={ state.decks } addingDeck={ state.addingDeck } />
+      <Sidebar
+        decks={ state.decks }
+        addingDeck={ state.addingDeck }
+        addDeck={name => store.dispatch(addDeck(name))}
+        showAddDeck={() => store.dispatch(showAddDeck())}
+        hideAddDeck={() => store.dispatch(hideAddDeck())}
+        />
     </App>,
     document.querySelector('#root')
   );
 }
-
 run();
 store.subscribe(run);
-window.show = () => store.dispatch(showAddDeck());
-window.hide = () => store.dispatch(hideAddDeck());
-window.add = () => store.dispatch(addDeck(new Date().toString()));
