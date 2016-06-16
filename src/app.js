d99c81d5044d688479f9b0bd90c383251bf47fed
drkchiloll <samwomack_@me.com>
2016-06-16 15:56:36 -0500

    split project into separate files: actions, reducers (for the redux portion) and separate individual components (App and Sidebar (for now)) into the Components directory

--- src/app.js
+++ src/app.js
@@ -1,94 +1,14 @@
-/*
- * ADD_DECK
- * SHOW_ADD_DECK
- * HIDE_ADD_DECK
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
 
-// ACTION CREATORS
-const addDeck = name => ({ type: 'ADD_DECK', data: name });
-const showAddDeck = () => ({ type: 'SHOW_ADD_DECK' });
-const hideAddDeck = () => ({ type: 'HIDE_ADD_DECK' });
+import { addDeck, showAddDeck, hideAddDeck } from './actions'
+import * as reducers from './reducers';
 
-const cards = (state, action) => {
-  switch(action.type) {
-    case 'ADD_CARD':
-      let newCard = Object.assign({}, action.data, {
-        score: 1,
-        id: +new Date
-      });
-      return state.concat([newCard]);
+const store = createStore(combineReducers(reducers));
 
-    default:
-      return state || [];
-  }
-};
-
-const decks = (state, action) => {
-  switch(action.type) {
-    case 'ADD_DECK':
-      let newDeck = { name: action.data, id: +new Date };
-      return state.concat([newDeck]);
-    default:
-      return state || [];
-  }
-};
-
-const addingDeck = (state, action) => {
-  switch(action.type) {
-    case 'SHOW_ADD_DECK':
-      return true;
-    case 'HIDE_ADD_DECK':
-      return false;
-    default:
-      return !!state;
-  }
-}
-
-const store = Redux.createStore(Redux.combineReducers({
-  cards,
-  decks,
-  addingDeck
-}));
-
-const App = (props) => {
-
-  return (
-    <div className='app'>
-      { props.children }
-    </div>
-  );
-};
-
-class Sidebar extends React.Component {
-  componentDidUpdate() {
-    var el = this.refs.add;
-    if(el) el.focus();
-  }
-  render() {
-    let props = this.props;
-    return (
-      <div className='sidebar'>
-        <h2>All Decks</h2>
-        <button onClick={ e => this.props.showAddDeck() }>
-          New Deck
-        </button>
-        <ul>
-          {props.decks.map((deck, i) =>
-            <li key={i}> { deck.name } </li>
-          )}
-        </ul>
-        { props.addingDeck &&
-          <input ref='add' onKeyPress={this.createDeck.bind(this)} /> }
-      </div>
-    )
-  }
-  createDeck(evt) {
-    if(evt.which !== 13) return;
-    var name = this.refs.add.value;
-    this.props.addDeck(name);
-    this.props.hideAddDeck();
-  }
-}
+import App from './components/App';
+import Sidebar from './components/Sidebar';
 
 function run() {
   let state = store.getState();
