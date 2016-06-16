590eee0c1e9f51b9865ebe24d3e14cee8455ba79
drkchiloll <samwomack_@me.com>
2016-06-16 15:57:29 -0500

    individual functionality for Redux based on functionality

---
+++ src/actions.js
@@ -0,0 +1,9 @@
+/*
+ * ADD_DECK
+ * SHOW_ADD_DECK
+ * HIDE_ADD_DECK
+ */
+// ACTION CREATORS
+export const addDeck = name => ({ type: 'ADD_DECK', data: name });
+export const showAddDeck = () => ({ type: 'SHOW_ADD_DECK' });
+export const hideAddDeck = () => ({ type: 'HIDE_ADD_DECK' });
