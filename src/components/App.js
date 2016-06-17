1a1eb96f4b6c1c94acb31185248d5ed5dd3e6ef7
drkchiloll <samwomack_@me.com>
2016-06-17 07:38:51 -0500

    imported Sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Sidebar from './Sidebar';
 
 export default function App(props) {
   return (
     <div className='app'>
+      <Sidebar />
       { props.children }
     </div>
   );
 };
-
-// export default App;
