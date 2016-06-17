8f0b33cda3dc596fdce10efb8562fe40d184d82c
drkchiloll <samwomack_@me.com>
2016-06-17 15:54:01 -0500

    Added NewCardModal as a Nested Route of VisibleCards Component

--- src/app.js
+++ src/app.js
@@ -13,13 +13,16 @@ import * as localStore from './localStore';
 
 import App from './components/App';
 import VisibleCards from './components/VisibleCards';
+import NewCardModal from './components/NewCardModal';
 
 const store = createStore(combineReducers(reducers, localStore.get()));
 const history = syncHistoryWithStore(browserHistory, store);
 
 const route = (
   <Route path='/' component={App}>
-    <Route path='/deck/:deckId' component={VisibleCards} />
+    <Route path='/deck/:deckId' component={VisibleCards}>
+      <Route path='/deck/:deckId/new' component={NewCardModal}/>
+    </Route>
   </Route>
 );
 
