31a41495286a4820daf699a0693361ccfd0eeb76
drkchiloll <samwomack_@me.com>
2016-06-17 07:38:29 -0500

    Implemented react-router and react-router-redux..had to move Sidebar Component into the Main App Component as it is a child of the App Component

--- src/app.js
+++ src/app.js
@@ -3,21 +3,26 @@ import ReactDOM from 'react-dom';
 
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
 import * as reducers from './reducers';
 
-const store = createStore(combineReducers(reducers));
+reducers.routing = routerReducer;
 
 import App from './components/App';
-import Sidebar from './components/Sidebar';
+
+const store = createStore(combineReducers(reducers));
+const history = syncHistoryWithStore(browserHistory, store);
 
 function run() {
   let state = store.getState();
   console.log(state);
   ReactDOM.render(
     <Provider store={store}>
-      <App>
-        <Sidebar />
-      </App>
+      <Router history={history}>
+        <Route path='/' component={App}></Route>
+      </Router>
     </Provider>,
     document.querySelector('#root')
   );
