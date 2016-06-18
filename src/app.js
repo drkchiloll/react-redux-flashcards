bcf9372f05272dc3fdde9aa22cc7e336658448c6
drkchiloll <samwomack_@me.com>
2016-06-18 14:53:39 -0500

    refactored to support external api server

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
-
+import thunkMiddleware from 'redux-thunk';
+import { fetchData } from './actions';
 import * as reducers from './reducers';
 
 reducers.routing = routerReducer;
-import * as localStore from './localStore';
 
 import App from './components/App';
 import VisibleCards from './components/VisibleCards';
@@ -17,7 +17,7 @@ import NewCardModal from './components/NewCardModal';
 import EditCardModal from './components/EditCardModal';
 import StudyModal from './components/StudyModal';
 
-const store = createStore(combineReducers(reducers), localStore.get());
+const store = createStore(combineReducers(reducers), applyMiddleware(thunkMiddleware));
 const history = syncHistoryWithStore(browserHistory, store);
 
 const route = (
@@ -32,7 +32,6 @@ const route = (
 
 function run() {
   let state = store.getState();
-  localStore.set(state, ['decks', 'cards']);
   ReactDOM.render(
     <Provider store={store}>
       <Router history={history}>
@@ -42,5 +41,24 @@ function run() {
     document.querySelector('#root')
   );
 }
-run();
-store.subscribe(run);
+
+function save() {
+  var state = store.getState();
+  fetch('/api/data', {
+    method: 'POST',
+    headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      decks: state.decks,
+      cards: state.cards
+    })
+  });
+}
+
+function init() {
+  run();
+  store.subscribe(run);
+  store.subscribe(save);
+  store.dispatch(fetchData())
+}
+
+init();
